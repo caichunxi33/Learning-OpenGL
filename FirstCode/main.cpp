9d7ee93cf19471736ec222842b2d002973339307
caichunxi <caichunxi33@sina.com>
2017-07-07 21:51:40 +0800

    Initial Commit

---
+++ FirstCode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FirstCode
+//
+//  Created by 蔡春锡 on 2017/7/7.
+//  Copyright © 2017年 蔡春锡. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
